7c4f3c2ec352463292444cab3b5c3a0e302741e5
Stanislav Sidelnikov <sidelnikov.stanislav@gmail.com>
2016-02-18 09:12:30 +0500

    Initial Commit

---
+++ UCI9DC L56 To Do List/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  UCI9DC L56 To Do List
+//
+//  Created by Stanislav Sidelnikov on 18/02/16.
+//  Copyright © 2016 Stanislav Sidelnikov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ UCI9DC L56 To Do List/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  UCI9DC L56 To Do List
+//
+//  Created by Stanislav Sidelnikov on 18/02/16.
+//  Copyright © 2016 Stanislav Sidelnikov. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
